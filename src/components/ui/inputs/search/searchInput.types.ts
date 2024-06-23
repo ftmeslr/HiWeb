2fa541b66c897ea795805a0124c54f8598a17395
f.salarian <ftme.slr@gmail.com>
2024-06-23 13:31:52 +1245

    [fix]:check build errors

--- src/components/ui/inputs/search/searchInput.types.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-export type IMainInputProps = {
-  hasFilter?: boolean | undefined;
-  onFilterClick?: () => void | undefined;
-  filterIcon?: ReactNode | undefined;
-};
